6d3878c0dc1c02b1ad27b0cd5013b6123acd080c
Maria <jm1726@yandex.ru>
2026-08-05 22:11:12 +0300

    add exit command and loop in temp.py

--- temp.py
+++ temp.py
@@ -8,18 +8,25 @@ This is a temporary script file.
 HELP = """
 help - напечатать справку по программе.
 add - добавить задачу в список (название задачи запрашиваем у пользователя).
-show - напечатать все добавленные задачи."""
+show - напечатать все добавленные задачи.
+exit - закрыть программу."""
 
 tasks = []
 
-command = input("Введите команду: ")
-if command == "help":
-    print(HELP)
-elif command == "show":
-    print(tasks)
-elif command == "add":
-    task = input("Введите название задачи: ")
-    tasks.append(task)
-    print("Задача добавлена")
-else:
-    print("Неизвестная команда")
+while True:
+    command = input("Введите команду: ")
+    if command == "help":
+        print(HELP)
+    elif command == "show":
+        print(tasks)
+    elif command == "add":
+        task = input("Введите название задачи: ")
+        tasks.append(task)
+        print("Задача добавлена")
+    elif command == "exit":
+        print("Спасибо за использование! До свидания!")
+        break
+    else:
+        print("Неизвестная команда. Введите валидную команду.")
+        continue
+        
