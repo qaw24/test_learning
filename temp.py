b2430da15aa8d6245cb2bb2a7ad17a1d4129efc1
Maria <jm1726@yandex.ru>
2026-08-05 22:24:45 +0300

    add dates for tasks in temp.py

--- temp.py
+++ temp.py
@@ -11,18 +11,28 @@ add - добавить задачу в список (название задач
 show - напечатать все добавленные задачи.
 exit - закрыть программу."""
 
-tasks = []
+today = []
+tomorrow = []
+other = []
 
 while True:
     command = input("Введите команду: ")
     if command == "help":
         print(HELP)
     elif command == "show":
-        print(tasks)
+        print("Сеогдня\n", today)
+        print("Завтра\n", tomorrow)
+        print("Другие\n", other)
     elif command == "add":
+        date = input("Введите дату: ")
         task = input("Введите название задачи: ")
-        tasks.append(task)
-        print("Задача добавлена")
+        if date == "Сегодня":
+            today.append(task)
+        elif date == "Завтра":
+            tomorrow.append(task)
+        else:
+            other.apend(task)
+        print(f"Задача {task} добавлена")
     elif command == "exit":
         print("Спасибо за использование! До свидания!")
         break
